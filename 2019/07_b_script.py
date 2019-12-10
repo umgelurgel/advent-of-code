ff1cf6614efb8d67e9e19bf470ee26ce4278c1c8
umgelurgel <umgelurgel@gmail.com>
2019-12-10 14:04:36 +0100

    Working day 7 b.

--- 2019/07_b_script.py
+++ 2019/07_b_script.py
@@ -22,7 +22,7 @@ def get_operand(mode, index, registers):
 
 
 def process_input(phase, program_input, registers, index):
-    print(f'processing with {phase} {program_input}')
+    print(f'processing with phase: {phase}, input: {program_input}, index: {index}')
     ADD_OP = 1
     MULT_OP = 2
     SAVE_OP = 3
@@ -37,8 +37,8 @@ def process_input(phase, program_input, registers, index):
     the_output = None
 
     while True:
-        print(f'{index} out of {len(registers)-1}')
-        print(registers)
+        # print(f'{index} out of {len(registers)-1}')
+        # print(registers)
 
         # zero pad the instruction
         instruction = f'{registers[index]:05}'
@@ -51,7 +51,7 @@ def process_input(phase, program_input, registers, index):
         if opcode == BREAK_OP:
             print(f'break opcode found, returning {the_output}, {index+1}')
 
-            return the_output, index+1
+            return the_output, -1
             break
         elif opcode == ADD_OP:
             left = get_operand(first_param_mode, index+1, registers)
@@ -78,6 +78,8 @@ def process_input(phase, program_input, registers, index):
             # return registers[index + 1]
             the_output = registers[output_ix]
             index += 2
+            return the_output, index
+
         elif opcode == JUMP_TRUE_OP:
             condition = get_operand(first_param_mode, index+1, registers)
             instruction_pointer = get_operand(second_param_mode, index+2, registers)
@@ -119,15 +121,15 @@ if __name__ == '__main__':
     with open("07_input.txt","r") as file:
         lines = file.read().split('\n')[0]
 
-    lines='3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5'
+    # lines='3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10'
     registers = [int(x) for x in lines.split(',')]
 
     out_max = None
     option_max = []
 
-    # phase_options = list(permutations([5,6,7,8,9]))
-    # for option in phase_options:
-    for option in [(9,8,7,6,5)]:
+    phase_options = list(permutations([5,6,7,8,9]))
+    for option in phase_options:
+    # for option in [(9,8,7,6,5)]:
         print(f'Processing phase setting: {option}')
 
         ix_a, ix_b, ix_c, ix_d, ix_e = 0, 0, 0, 0, 0
@@ -138,6 +140,8 @@ if __name__ == '__main__':
             out_a, ix_a = process_input(
                 phase=option[0] if first_run else out_e, program_input=out_e, registers=reg_a, index=ix_a
             )
+            if ix_a == -1:
+                break
             out_b, ix_b = process_input(
                 phase=option[1] if first_run else out_a, program_input=out_a, registers=reg_b, index=ix_b
             )
@@ -152,11 +156,9 @@ if __name__ == '__main__':
             )
 
             first_run = False
-            if ix_e >= len(reg_e) - 1:
-                break
 
         if not out_max or out_e > out_max:
             out_max = out_e
             option_max = option
 
-    print(f'max signal: {out_max}, option: {option_max}')
+    print(f'max signal: {out_max} option: {option_max}')
